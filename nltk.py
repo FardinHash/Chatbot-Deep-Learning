e3aa72e272b9edbf8f29904ffba72b35610d8870
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-30 00:23:03 +0600

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -19,8 +19,10 @@ def stem(word):
 #bagging
 def bag_of_words(tokenized_sentence, words):
     sentence_words = [stem(word) for word in tokenized_sentence]
+    
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
+    
     for idx, w in enumerate(words):
         if w in sentence_words:
             bag[idx] = 1
