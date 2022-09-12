ac94d36d1cf4a707db288f3b9425f72d6e3275ab
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-13 00:06:43 +0600

    Update train.py

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from torch.utils.data import Dataset, DataLoader
 from nltk import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('intents.json', 'r') as f:
-    intents = json.load(f)
+with open('intents.json', 'r') as f:  #data load
+    intents= json.load(f)
 
 all_words = []
 tags = []
