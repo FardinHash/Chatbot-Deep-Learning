bec5e9ba873a67f5c64b29eef3b8940b022ee86c
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-12 22:19:21 +0600

    Update nltk.py

--- nltk.py
+++ nltk.py
@@ -8,7 +8,7 @@ import nltk
 nltk.download('punkt')
 from nltk import stem
 
-stemmer = stem.PorterStemmer()
+stemmer= stem.PorterStemmer()
 
 #tokenization
 def tokenize(sentence):
@@ -20,13 +20,13 @@ def stem(word):
 
 #bagging
 def bag_of_words(tokenized_sentence, words):
-    sentence_words = [stem(word) for word in tokenized_sentence]
+    sentence_words= [stem(word) for word in tokenized_sentence]
     
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
     
     for idx, w in enumerate(words):
         if w in sentence_words:
-            bag[idx] = 1
+            bag[idx]= 1
 
     return bag
