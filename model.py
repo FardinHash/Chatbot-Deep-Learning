b582568f16cc78679869ea3ea09ccddcbc08ba2c
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-12 22:17:48 +0600

    Update model.py

--- model.py
+++ model.py
@@ -6,8 +6,11 @@ import torch
 import torch.nn as nn
 
 
+#creating neural network
+
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
+        
         super(NeuralNet, self).__init__()
         self.l1 = nn.Linear(input_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, hidden_size)
